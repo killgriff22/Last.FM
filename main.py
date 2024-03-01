74690b2aa446dd9a2f49e84c0167f9e5a8544646
killgriff22 <kilgrif22033@gmail.com>
2024-03-01 10:49:24 -0500

    wrong var

--- main.py
+++ main.py
@@ -77,5 +77,5 @@ async def set_presence():
     else:
         await client.change_presence(status=discord.Status.online, activity=None)
         print("No song playing")
-        song = None
+        last_song = None
 client.run(TOKEN)
